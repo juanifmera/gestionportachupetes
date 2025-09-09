904ad8a272773573d421cc7d31be97444a59f83e
juanifmera <juanignaciofmera@gmail.com>
2025-09-09 00:07:34 -0300

    Twelveth Commit. Modificacion del archivo pedidos dentro de la carpeta CRUD. Generar logica para creacr, modificar, leer y cambiar el estado de los pedidos. En base a cada accion, cada una de ellas desencadenara otras acciones, por ejemplo, antes de generar un nuevo pedido, se verificara que el pedido puede generarse con el stock actual. En ese caso se genera el pedido y se descuenta el stock.

--- sistema_portachupetes/crud/pedidos.py
+++ sistema_portachupetes/crud/pedidos.py
@@ -3,5 +3,12 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from database.models import Stock, Material, Pedido
 
+#Continuar con la Creacion de las siguientes Funciones para los pedidos
 def generar_pedido():
+    pass
+
+def cambiar_estado():
+    pass
+
+def modificar_pedido():
     pass
