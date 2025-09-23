e754e6f025afde927ae5d29d3b2cf87a2f1f6980
Juan Mera <juanignaciofmera@gmail.com>
2025-09-22 22:10:35 -0300

    Ready

--- sistema_portachupetes/pages/3_📝_Pedidos.py
+++ sistema_portachupetes/pages/3_📝_Pedidos.py
@@ -283,7 +283,7 @@ with tabs_pedido[4]:
         st.warning("❌ No hay Pedidos disponibles con los filtros seleccionados.")
     else:
         df_filtrado["Costo Total"] = df_filtrado['ID'].apply(lambda x: calcular_costo_total_pedido(x))
-        st.dataframe(df_filtrado[["ID", "Cliente", "Teléfono", "Fecha Creación", "Estado", "Costo Total"]], width='stretch')
+        st.dataframe(df_filtrado[["ID", "Cliente", "Telefono", "Fecha Creación", "Estado", "Costo Total"]], width='stretch')
         st.info(f'Se encontraron {df_filtrado.shape[0]} registros para su búsqueda')
 
 ## VISUALIZAR MATERIALES POR PEDIDO ##
