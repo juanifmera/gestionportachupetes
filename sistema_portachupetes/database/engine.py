be5b3db44fd4a06c7380ad484831a0f984c1ecee
Juan Mera <juanignaciofmera@gmail.com>
2025-09-29 23:35:01 -0300

    Implementacion tab para pedidos mayoristas

--- sistema_portachupetes/database/engine.py
+++ sistema_portachupetes/database/engine.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
 import streamlit as st
 
-#engine = create_engine(st.secrets["DATABASE_URL"])
-engine = create_engine('postgresql://juanifmera:8SJD1tajd64ZkOKmFns55LDsQ0eycJ6e@dpg-d36vj3ggjchc73br6he0-a.oregon-postgres.render.com/udibaby')
+engine = create_engine(st.secrets["DATABASE_URL"])
