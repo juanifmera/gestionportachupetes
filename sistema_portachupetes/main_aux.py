8bb61a68dedce9a634afe9a001dfb191f34292f3
Juan Mera <juanignaciofmera@gmail.com>
2025-10-11 18:27:25 -0300

    Actualizacion para generar pedidos con llavero e identificadores

--- sistema_portachupetes/main_aux.py
+++ sistema_portachupetes/main_aux.py
@@ -1,14 +1,14 @@
 from database.engine import engine
 from sqlalchemy.orm import Session
 from crud.materiales import eliminar_material
-from crud.stock import eliminar_stock, agregar_stock
+from crud.stock import eliminar_stock, agregar_stock, listar_stock
 from crud.pedidos import crear_pedido_dummy
 from database.models import MaterialPedido, Material, Stock
 
 try:
     session = Session(bind=engine)
     
-    print(crear_pedido_dummy(
+    '''print(crear_pedido_dummy(
         cliente="EJEMPLO",
         materiales_portachupete={
             "broche": "SBLA",
@@ -21,7 +21,11 @@ try:
                         {"codigo": "LBEI", "cantidad": 2}],
         },
         telefono="123456789",
-    ))
+    ))'''
+    
+    results = session.query(Stock).all()
+    for result in results:
+        print(result.codigo_material)
 
 except Exception as e:
     print(e)
